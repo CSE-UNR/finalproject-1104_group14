47a7044b16aee15a51d86db489807293f6b2a0ae
jjc <jjc@nevada.unr.edu>
2024-05-02 18:38:32 +0000

    general framework

--- images.c
+++ images.c
@@ -3,7 +3,55 @@
 //Assignment: Final Project
 
 #include <stdio.h>
+#include <stdbool.h>
+
+int menu();
+int editMenu();
+void display(int imageArray);
+int crop(int imageArray);
+int dim(int imageArray);
+int brighten(int imageArray);
+int rotate(int imageArray);
+// save function
 
 int main(){
+
+	/*
+	menuChoice = menu();
+	switch(menuChoice){
+		case 1:
+			//loading stuff
+			break;
+		case 2:
+			display();
+			break;
+		case 3:
+			editChoice = editMenu();
+			switch(editChoice){
+				
+			}
+			break;
+			save();
+		case 0:
+			// say goodbye
+			break;
+		default:
+			//invalid
+			break;
+	}
+	*/
+	
+	//loop[START] (while menu() != 0)
+	//menu()
+		//load
+		//display
+		//edit -> editMenu()
+			//crop
+			//dim
+			//brighten
+			//rotate
+		//save?
+	//loop[END]
+
 	return 0;
 }
