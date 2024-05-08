1c26e6aa52e63f70e1bebaf1bec6d9514e74fe15
Aidan Chauvin <achauvin@nevada.unr.edu>
2024-05-08 06:38:04 +0000

    Final Result

--- images.c
+++ images.c
@@ -10,14 +10,13 @@
 int menu();
 int editMenu();
 void load(int inputRows, int inputCols, char arrayInput[][inputCols], int *outputRows, int *outputCols);
-void crop(char imageArray);
+void crop(int inputRows, int inputCols, char arrayInput[][inputCols]);
 char dim(char inputValue);
 char brighten(char inputValue);
-int rotate(char imageArray);
 void save(int inputRows, int inputCols, char arrayInput[][inputCols]);
 
 int main(){
-	char imageArray[MAX_ROWS][MAX_COLS];
+	char imageArray[MAX_ROWS][MAX_COLS], newImage[MAX_ROWS][MAX_COLS];
 	int rows, cols, menuChoice = 1, editChoice = 1;
 	int fileLoaded = 0;
 	while(menuChoice != 0){
@@ -26,12 +25,13 @@ int main(){
 			case 1:
 				load(MAX_ROWS, MAX_COLS, imageArray, &rows, &cols);
 				fileLoaded = 1;
+				newImage[MAX_ROWS][MAX_COLS] = imageArray[MAX_ROWS][MAX_COLS];
 				break;
 			case 2:
 				if(fileLoaded == 0){
 					printf("Error: Image not loaded.\n");
 				} else if (fileLoaded == 1){
-					for (int i = 0; i < rows; i++) {
+					for (int i = 0; i <= rows; i++) {
     						for (int j = 0; j < cols; j++) {
         						if(imageArray[i][j] == '0'){
         							printf(" ");
@@ -55,14 +55,51 @@ int main(){
 				} else if (fileLoaded == 1) {
 					editChoice = editMenu();
 					switch(editChoice){
+						case 1:
+							for (int i = 0; i <= rows; i++) {
+    								for (int j = 0; j < cols; j++) {
+        								if(imageArray[i][j] == '0'){
+        									printf(" ");
+        								} else if (imageArray[i][j] == '1'){
+        									printf(".");
+        								} else if (imageArray[i][j] == '2'){
+        									printf("o");
+        								} else if (imageArray[i][j] == '3'){
+        									printf("O");
+        								} else if (imageArray[i][j] == '4'){
+        									printf("0");
+        								}
+    								}
+    								printf("\n");
+							}
+							crop(rows, cols, imageArray);
+							break;
 						case 2:
 							for (int i = 0; i < rows; i++) {
     								for (int j = 0; j < cols; j++) {
         								if(imageArray[i][j] != '0'){
         									imageArray[i][j] = dim(imageArray[i][j]);
+        									
         								}
     								}
 							}
+							for (int i = 0; i <= rows; i++) {
+    								for (int j = 0; j < cols; j++) {
+        								if(imageArray[i][j] == '0'){
+        									printf(" ");
+        								} else if (imageArray[i][j] == '1'){
+        									printf(".");
+        								} else if (imageArray[i][j] == '2'){
+        									printf("o");
+        								} else if (imageArray[i][j] == '3'){
+        									printf("O");
+        								} else if (imageArray[i][j] == '4'){
+        									printf("0");
+        								}
+    								}
+    								printf("\n");
+							}
+							save(rows, cols, imageArray);
 							break;
 						case 3:
 							for (int i = 0; i < rows; i++) {
@@ -72,11 +109,26 @@ int main(){
         								}
     								}
 							}
-							break;
+							for (int i = 0; i <= rows; i++) {
+    								for (int j = 0; j < cols; j++) {
+        								if(imageArray[i][j] == '0'){
+        									printf(" ");
+        								} else if (imageArray[i][j] == '1'){
+        									printf(".");
+        								} else if (imageArray[i][j] == '2'){
+        									printf("o");
+        								} else if (imageArray[i][j] == '3'){
+        									printf("O");
+        								} else if (imageArray[i][j] == '4'){
+        									printf("0");
+        								}
+    								}
+    								printf("\n");
+							}
+							save(rows, cols, imageArray);
 							break;
 					}
-				}
-				//save(rows, cols, imageArray);
+				
 				break;
 			case 0:
 				printf("Goodbye!\n\n");
@@ -86,7 +138,7 @@ int main(){
 				break;
 		}
 	}
-	
+	}
 	return 0;
 }
 
@@ -186,4 +238,77 @@ char brighten(char inputValue){
 			break;
 	}
 	return outputValue;
+}
+
+void save(int inputRows, int inputCols, char arrayInput[][inputCols]){
+	char choice;
+	char newFileName[100];
+	FILE* fptr;
+	
+	printf ("Would you like to save the file? (y/n) ");
+	scanf (" %c", &choice);
+	
+	if (choice == 'y' || choice == 'Y'){
+		printf ("What do you want to name the image file? (include the extension) ");
+		scanf("%s", newFileName);
+		
+		fptr = fopen (newFileName, "w");
+		if (fptr == NULL){
+			printf("Error opening file.\n");
+		}
+		
+		else{
+			for (int index1 = 0; index1 < inputRows; index1++){
+				for (int index2 = 0; arrayInput[index1][index2] != '\0'; index2++){
+					fprintf (fptr, "%c", arrayInput[index1][index2]);
+				}
+				fprintf (fptr, "\n");
+			}
+			fclose (fptr);
+		}
+	}
+}
+
+void crop(int inputRows, int inputCols, char arrayInput[][inputCols]){
+	char choice;
+	int newLeftCol, newRightCol, newTopRow, newBottomRow, base, height;
+	
+	printf("The image you want to crop is %d x %d\n", inputRows, inputCols);
+	printf("The row and column values start in the upper lefthand corner.\n");
+	printf("\nwhich column do you want to be the new left side? ");
+	scanf("%d", &newLeftCol);
+	printf("\nWhich column do you want to be the new right side? ");
+	scanf("%d", &newRightCol);
+	printf("\nWhich row do you want to be the new top? ");
+	scanf("%d", &newTopRow);
+	printf("\nWich row do you want to be the new botom? ");
+	scanf("%d", &newBottomRow);
+	
+	base = newRightCol - newLeftCol + 1;
+	height = newBottomRow - newTopRow + 1;
+	
+	char newImage[height][base];
+	
+	for (int index1 = 0; index1 < height; index1++){
+		for (int index2 = 0; index2 < base; index2++){
+			newImage[index1][index2] = arrayInput[index1+(newTopRow-1)][index2+(newLeftCol-1)];
+		}
+	}
+	for (int i = 0; i < height; i++) {
+    		for (int j = 0; j < base; j++) {
+        		if(newImage[i][j] == '0'){
+        			printf(" ");
+        		} else if (newImage[i][j] == '1'){
+        			printf(".");
+        		} else if (newImage[i][j] == '2'){
+        			printf("o");
+        		} else if (newImage[i][j] == '3'){
+        			printf("O");
+        		} else if (newImage[i][j] == '4'){
+        			printf("0");
+        		}
+    		}
+    		printf("\n");
+	}
+	save(height, base, newImage);
 }
